2579bb6a87973c93f2dc1b9d4d9a2d157339ca30
Adeife Taiwo <adeifetaiwo50@gmail.com>
2023-09-14 10:25:00 +0100

    quick change

--- lib/detail_scree/details_screen.dart
+++ lib/detail_scree/details_screen.dart
@@ -20,39 +20,54 @@ class _DetailScreenState extends State<DetailScreen> with SingleTickerProviderSt
     super.initState();
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 2),
+      duration: const Duration(seconds: 1),
       lowerBound: 0,
       upperBound: 1
     );
+    _animationController.forward();
+  }
+
+  @override
+  void dispose(){
+    _animationController.dispose();
+    super.dispose();
   }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(0.0),
-        child: Container(
-            width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-            padding: const EdgeInsets.all(20.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12.0),
-              image: const DecorationImage(image: AssetImage('assets/images/background_2.jpg'), fit: BoxFit.cover)
-            ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Hero(
-                tag: widget.userModel.avatar!,
-                child: CircleAvatar(maxRadius: 60,
-                backgroundImage: NetworkImage(widget.userModel.avatar!),
-                ),
-              ),
-              const SizedBox(height: 50),
+      appBar: AppBar(
+        
+      ),
+      body: AnimatedBuilder(
+        animation: _animationController,
 
-              Text("${widget.userModel.firstName!} ${widget.userModel.lastName!}",
-              style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),)
-            ],
+        builder: (context, child) => Padding(
+          padding: EdgeInsets.only(
+            top: 100- _animationController.value *100,
+          ),
+          child: Container(
+              width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+              padding: const EdgeInsets.all(20.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12.0),
+                image: const DecorationImage(image: AssetImage('assets/images/background_2.jpg'), fit: BoxFit.cover)
+              ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Hero(
+                  tag: widget.userModel.avatar!,
+                  child: CircleAvatar(maxRadius: 60,
+                  backgroundImage: NetworkImage(widget.userModel.avatar!),
+                  ),
+                ),
+                const SizedBox(height: 50),
+
+                Text("${widget.userModel.firstName!} ${widget.userModel.lastName!}",
+                style: const TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 20),)
+              ],
+            ),
           ),
         ),
       ),
